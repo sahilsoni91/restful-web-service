f42bc8d0528a628ed299578a2eb7bc47e7aa1dda
sahilsoni91 <sahilsoni91@gmail.com>
2020-06-14 17:58:31 +0530

    Path Variable Service

--- src/main/java/com/rest/webservice/restfulwebservice/controller/HelloWorldController.java
+++ src/main/java/com/rest/webservice/restfulwebservice/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest.webservice.restfulwebservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean("Hello World, " + name);
 	}
 }
