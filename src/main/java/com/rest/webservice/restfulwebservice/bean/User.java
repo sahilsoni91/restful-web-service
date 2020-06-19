17cf85c564132d274a194b30b438c179bb8eb758
sahilsoni91 <sahilsoni91@gmail.com>
2020-06-19 18:43:49 +0530

    Enhancing Documentation

--- src/main/java/com/rest/webservice/restfulwebservice/bean/User.java
+++ src/main/java/com/rest/webservice/restfulwebservice/bean/User.java
@@ -5,15 +5,20 @@ import java.time.LocalDate;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel(description = "Details about the user.")
 public class User {
 
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should have atleast 2 characters")
+	@ApiModelProperty(notes = "Name should atleast have 2 characters")
 	private String name;
 	
 	@Past(message = "Birthdate should be less than today's date")
+	@ApiModelProperty(notes = "Birthdate should be in the past")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
