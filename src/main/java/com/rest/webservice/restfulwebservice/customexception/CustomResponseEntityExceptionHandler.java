237d0079749fd984808bc640aa4f8f9f8b4bec53
sahilsoni91 <sahilsoni91@gmail.com>
2020-06-19 11:50:29 +0530

    Generic Exception Handler and common response for all exceptions

---
+++ src/main/java/com/rest/webservice/restfulwebservice/customexception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.rest.webservice.restfulwebservice.customexception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/*
+ * ControllerAdvice to share for all controllers i.e. share Exception handling for all controllers
+ */
+@ControllerAdvice
+@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+}
---
+++ src/main/java/com/rest/webservice/restfulwebservice/customexception/ExceptionResponse.java
@@ -0,0 +1,29 @@
+package com.rest.webservice.restfulwebservice.customexception;
+
+import java.time.LocalDateTime;
+
+public class ExceptionResponse {
+
+	private LocalDateTime timestamp;
+	private String message;
+	private String details;
+	
+	public ExceptionResponse(LocalDateTime timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public String getDetails() {
+		return details;
+	}
+}
