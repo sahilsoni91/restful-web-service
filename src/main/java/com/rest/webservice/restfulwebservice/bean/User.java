be385c8852ede119febc59b60e281b67b18bd2f9
sahilsoni91 <sahilsoni91@gmail.com>
2020-06-20 15:47:32 +0530

    JPA Support for User Service

--- src/main/java/com/rest/webservice/restfulwebservice/bean/User.java
+++ src/main/java/com/rest/webservice/restfulwebservice/bean/User.java
@@ -2,6 +2,9 @@ package com.rest.webservice.restfulwebservice.bean;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -10,8 +13,11 @@ import org.springframework.hateoas.RepresentationModel;
 /*
  * extends RepresentationModel to add link functionality for particular user
  */
+@Entity
 public class User extends RepresentationModel<User>{
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should have atleast 2 characters")
@@ -19,6 +25,10 @@ public class User extends RepresentationModel<User>{
 	
 	@Past(message = "Birthdate should be less than today's date")
 	private LocalDate birthDate;
+	
+	public User() {
+		super();
+	}
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		this.id = id;
---
+++ src/main/java/com/rest/webservice/restfulwebservice/controller/UserJPAController.java
@@ -0,0 +1,74 @@
+package com.rest.webservice.restfulwebservice.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.rest.webservice.restfulwebservice.bean.User;
+import com.rest.webservice.restfulwebservice.customexception.UserNotFoundException;
+import com.rest.webservice.restfulwebservice.dao.UserDaoService;
+import com.rest.webservice.restfulwebservice.dao.UserRepository;
+
+@RestController
+public class UserJPAController {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping(path = "jpa/users")
+	public List<User> retrieveAllUsers() {
+		List<User> users = userRepository.findAll();
+		for(User user : users) {
+			Link link = WebMvcLinkBuilder.linkTo(UserJPAController.class).slash(user.getId()).withSelfRel();
+			user.add(link);
+		}
+		return users;
+	}
+	
+	@GetMapping(path = "jpa/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		/*
+		 * Optional returns a proper object
+		 */
+		Optional<User> user = userRepository.findById(id);
+		if(!user.isPresent()) {
+			throw new UserNotFoundException("id : " + id);
+		}
+		return user.get();
+	}
+	
+	@PostMapping(path = "jpa/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		User savedUser = userRepository.save(user);
+		/*
+		 * If only CREATED status code to be returned in Response
+		 */
+//		return ResponseEntity.status(HttpStatus.CREATED).build();
+		
+		/*
+		 * If URI of added resource to send in Response with CREATED status code
+		 */
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping(path = "jpa/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		userRepository.deleteById(id);
+	}
+}
